bc29778a0dcfe1d25d06384c98b332dba00e4c55
Anton Harnik <ajgarnik@gmail.com>
2026-07-08 23:01:41 +0300

    updates

--- sslocal.sh
+++ sslocal.sh
@@ -7,46 +7,57 @@
 # Other Linux distros may have other configurations so this script may need changes
 
 SERVER="localhost"
-CONFIG_DIR="~/.secrets"
-CONFIG_PREFIX="outline-"
-CONFIG=$CONFIG_DIR/$CONFIG_PREFIX-$SERVER
+CONFIG_PREFIX="$HOME/.secrets/outline-"
+LISTEN_ADDR="127.0.0.1"
+LISTEN_PORT=1080
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -s|--server)
       SERVER=$2
-      CONFIG=$CONFIG_DIR/$CONFIG_PREFIX-$SERVER
-      shift 2
-      ;;
-    -d|--config-dir)
-      CONFIG_DIR=$2
-      CONFIG=$CONFIG_DIR/$CONFIG_PREFIX-$SERVER
       shift 2
       ;;
     -p|--config-prefix)
       CONFIG_PREFIX=$2
-      CONFIG=$CONFIG_DIR/$CONFIG_PREFIX-$SERVER
       shift 2
+      ;;
     -c|--config)
       CONFIG=$2
-      shidt 2
+      shift 2
+      ;;
+    -la|--listen-addr)
+      LISTEN_ADDR_CLI=$2
+      shift 2
+      ;;
+    -lp|--listen-port)
+      LISTEN_PORT_CLI=$2
+      shift 2
+      ;;
     -*)
       echo "Unknown option: $1" >&2
       ;;
     *)
       SERVER="$1"
-      CONFIG=$CONFIG_DIR/$CONFIG_PREFIX-$SERVER
       shift
       ;;
+  esac
 done
 
+CONFIG="${CONFIG:-${CONFIG_PREFIX}${SERVER}}"
+echo "Using configuration from ${CONFIG}"
+
 . $CONFIG
-echo "Connecting to ${SERVER_IP}:${SERVER_PORT}"
+
+# listen address and port can be overridden by CL arguments
+LISTEN_ADDR=${LISTEN_ADDR_CLI:-${LISTEN_ADDR}}
+LISTEN_PORT=${LISTEN_PORT_CLI:-${LISTEN_PORT}}
+
+echo "Connecting to ${SERVER_IP}:${SERVER_PORT} / Listening on ${LISTEN_ADDR}:${LISTEN_PORT}"
 
 ss="ss://${USERNAME}:${PASSWORD}@${SERVER_IP}:${SERVER_PORT}"
 
 #ss-local -s $SERVER_IP -p $SERVER_PORT -k $PASSWORD -m $USERNAME -b 127.0.0.1 -l 1080
 go-shadowsocks2 \
   -c "$ss" \
-  -socks 127.0.0.1:1080 \
+  -socks $LISTEN_ADDR:$LISTEN_PORT \
   -verbose
